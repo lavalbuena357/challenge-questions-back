e4ad4007443cb07c64fade9921a71ed15626079f
cuervo357@gmail.com <cuervo357@gmail.com>
2022-01-04 11:57:40 -0500

    rutas y controller de preguntas

---
+++ src/Routes/controllers/questions.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { Question } = require('../../db.js')
+const router = express.Router()
+
+router.use(express.json())
+
+//crear una pregunta
+router.post('/', async (req, res) => {
+  const {question, levelId} = req.body
+  try {
+    const newQuestion = await Question.create({question, levelId})
+
+    return res.json(newQuestion)
+  } catch (err) {console.log(err)}
+})
+
+//obtener todas las preguntas
+router.get('/', async (req, res) => {
+  try {
+      let questions = await Question.findAll()
+      return res.json(questions)
+  } catch(err) {console.log(err)}
+});
+
+module.exports = router;
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -4,6 +4,7 @@ const historyRoute = require('./controllers/history.js')
 const usersRoute = require('./controllers/user.js')
 const levelsRoute = require('./controllers/levels.js')
 const prizesRoute = require('./controllers/prize.js')
+const questionsRoute = require('./controllers/questions.js')
 
 const router = express.Router()
 router.use(express.json())
@@ -12,5 +13,7 @@ router.use('/history', historyRoute)
 router.use('/users', usersRoute)
 router.use('/levels', levelsRoute)
 router.use('/prizes', prizesRoute)
+router.use('/questions', questionsRoute)
+
 
 module.exports = router
