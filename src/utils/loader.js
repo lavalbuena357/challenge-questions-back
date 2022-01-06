dd72288a05291ed46fc20d8d7909fdacac1a4c6b
cuervo357@gmail.com <cuervo357@gmail.com>
2022-01-06 01:51:13 -0500

    loader update

--- src/utils/loader.js
+++ src/utils/loader.js
@@ -1,48 +1,34 @@
 const express = require('express')
-const { User, Level, Prize, Question, Answer } = require('../db.js')
+const { Level, Prize, Question, Answer } = require('../db.js')
 const router = express.Router()
 
 router.use(express.json())
 
 //DATA LOADER
-const {users, levels, prizes, questions, answers} = require('./data.js')
+const {levels, prizes, questions, answers} = require('./data.js')
 
-users.forEach(el => User.findOrCreate({
-  where: {
-    name: el.name,
-    accum: el.accum,
-    level_reached: el.level_reached,
-    levelId: el.levelId,
-    prizeId: el.prizeId
-  }
-}))
+levels.forEach(el => Level.create({level: el.level}))
 
-levels.forEach(el => Level.findOrCreate({
-  where: {
-    level: el.level
-  }
-}))
-
-prizes.forEach(el => Prize.findOrCreate({
-  where: {
+prizes.forEach(el => Prize.create({
+  
     points: el.points,
     levelId: el.levelId
-  }
+ 
 }))
 
-questions.forEach(el => Question.findOrCreate({
-  where: {
+questions.forEach(el => Question.create({
+  
     question: el.question,
     levelId: el.levelId
-  }
+  
 }))
 
-answers.forEach(el => Answer.findOrCreate({
-  where: {
+answers.forEach(el => Answer.create({
+  
     answer: el.answer,
     is_correct: el.is_correct,
     questionId: el.questionId
-  }
+ 
 }))
 
 module.exports = router
