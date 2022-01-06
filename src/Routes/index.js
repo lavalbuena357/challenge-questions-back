1421fb9051d83fae26a8b6aa9aa1ad1cb74e5eec
cuervo357@gmail.com <cuervo357@gmail.com>
2022-01-06 01:47:01 -0500

    deshabilitar loaders

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -6,7 +6,7 @@ const levelsRoute = require('./controllers/levels.js')
 const prizesRoute = require('./controllers/prize.js')
 const questionsRoute = require('./controllers/questions.js')
 const answersRoute = require('./controllers/answers.js')
-const dataLoader = require('../utils/loader.js')
+//const dataLoader = require('../utils/loader.js')
 
 const router = express.Router()
 router.use(express.json())
@@ -17,7 +17,7 @@ router.use('/levels', levelsRoute)
 router.use('/prizes', prizesRoute)
 router.use('/questions', questionsRoute)
 router.use('/answers', answersRoute)
-router.use(dataLoader)
+//router.use(dataLoader)
 
 
 module.exports = router
